c4cb5bf2d642d545a8f799c3c1a604616f3485cd
steven-cpp <qwert556@live.cn>
2023-10-30 06:54:09 +0000

    feat: implement getConfHotspots

--- main.py
+++ main.py
@@ -28,19 +28,55 @@ Usage
 
 import lucene
 from query import *
+from rake_nltk import Rake
+# import matplotlib.pyplot as plt
 
 
 storeDir = "./index/"
 topK = 20
 searcher = Searher(store_dir = storeDir, topK = topK)
+    
+stop_words = ['abstract', 'approach', 'commitee', 'report', 'study']
 
 def getConfHostspots(conf: str, start: int, end=2023):
     confRes = searcher.searchByConf(conf, return_all=True, printing=False)
-    timeRes = searcher.searchByYearRange(start, end, return_all=True)
-    res = list(set(confRes) & set(timeRes))
-    return res
+    timeRes = searcher.searchByYearRange(start, end, return_all=True, printing=False)
+    
+    confUrl = {doc['url'] for doc in confRes if 'url' in doc}
+    timeUrl = {doc['url'] for doc in timeRes if 'url' in doc}
+    # Find the intersection of Urls
+    intersected_urls = timeUrl.intersection(confUrl)
+    
+    res = [doc for doc in confRes if doc['url'] in intersected_urls]
+    
+    titles = [doc['title'] for doc in res]
+    
+    print('*'*5 + ' Index Done, Start Keyword Extraction' + '*'*5)
+    rake = Rake(min_length=2, max_length=4)
+    rake.extract_keywords_from_sentences(titles)
+    keywords = rake.get_ranked_phrases()
+    
+    cnt = {}
+    for word in keywords:
+        if any(ele in word for ele in stop_words):
+            continue
+        if word in cnt:
+            cnt[word] += 1
+        else:
+            cnt[word] = 1
+            
+    cnt = sorted(cnt.items(), key=lambda item: item[1], reverse=True)
+    
+    return cnt
 
 
 if __name__ == "__main__":
-    res = getConfHostspots("AAAI", 2022)
-    print(len(res))
+    keywords = getConfHostspots("AAAI", 2022)
+    print(keywords[:10])
+    
+    
+    
+
+    
+    
+    
