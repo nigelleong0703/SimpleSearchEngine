e8867a7f25169ea8cb035f5a8cfc5c85be81ce31
steven-cpp <qwert556@live.cn>
2023-10-30 11:02:00 +0000

    fix: keyword count is null

--- main.py
+++ main.py
@@ -45,11 +45,20 @@ def describe(data: dict, title=None, xlabel=None, ylabel=None):
     plt.xticks(years, years)
     counts = {}
     for year in years:
+        keywords = [item[0] for item in data[years[-1]]]
         for item in data[year]:
             if item[0] in counts:
                 counts[item[0]].append(item[1])
             else:
                 counts[item[0]] = [item[1]]
+            keywords.remove(item[0])
+        if len(keywords):
+            for key in keywords:
+                if key in counts:
+                    counts[key].append(0)
+                else:
+                    counts[key] = [0]
+        
     
     for key, cnts in counts.items():
         plt.plot(years, cnts, label=key)
@@ -62,18 +71,18 @@ def describe(data: dict, title=None, xlabel=None, ylabel=None):
     plt.show()
 
 
-def getPivotAuthors():
-    pass
+def getPivotAuthors(conf: str, keyword: str, start: int, end=2023):
+    
+    
     
 
-def getConfHotspotsEvo(conf: str, start: int, end=2023, tok_k=5):
-    confRes = searcher.searchByConf(conf, return_all=True, printing=False)
-    keywords_glob = _getConfHotspots(confRes, start, end)[:tok_k]
+def getConfHotspotsEvo(conf: str, start: int, end=2023, top_k=5):
+    keywords_glob = _getConfHotspots(conf, start, end)[:top_k]
     res = {}
     print('*'*10, 'Yearly Conference Hostspots Summary', '*'*10)
     print('Research hotspots since', start, 'to', end, 'are', keywords_glob)
     for i in range(start, end + 1):
-        keywords = _getConfHotspots(confRes, i, i)
+        keywords = _getConfHotspots(conf, i, i)
         res[i] = _intersect(keywords, keywords_glob)
         print(str(i) + ':', res[i])
     return res
@@ -85,25 +94,17 @@ def _intersect(src: list, filter: list):
 
 
 def getConfHotspots(conf: str,  start: int, end=2023, top_k=5):
-    confRes = searcher.searchByConf(conf, return_all=True, printing=False)
     keywordsMap = {}
     print('*'*10, 'Keywords Summary', '*'*10)
     for i in range(start, end + 1):
-        keywords = _getConfHotspots(confRes, i, i)
+        keywords = _getConfHotspots(conf, i, i)
         keywordsMap[i] = keywords[:top_k]
         print(i, ":", keywordsMap[i])
     return keywordsMap
      
 
-def _getConfHotspots(confRes: list, start: int, end=2023):
-    timeRes = searcher.searchByYearRange(start, end, return_all=True, printing=False)
-    
-    confUrl = {doc['url'] for doc in confRes if 'url' in doc}
-    timeUrl = {doc['url'] for doc in timeRes if 'url' in doc}
-    # Find the intersection of Urls
-    intersected_urls = timeUrl.intersection(confUrl)
-    
-    res = [doc for doc in confRes if doc['url'] in intersected_urls]
+def _getConfHotspots(conf: str, start: int, end=2023):
+    res = searcher.multiFieldSearch(start, end, conf, return_all=True, printing=False)
     
     titles = [doc['title'] for doc in res]
     
@@ -128,12 +129,11 @@ def _getConfHotspots(confRes: list, start: int, end=2023):
 
 if __name__ == "__main__":
     # keywords = getConfHotspots("AAAI", 2020)
-    data = {2020: [('reinforcement learning', 13), ('supervised learning', 13), ('task learning', 11), ('machine learning', 8), ('shot learning', 8)],
-            2021: [('supervised learning', 15), ('shot learning', 15), ('reinforcement learning', 14), ('task learning', 11), ('machine learning', 9)],
-            2022: [('supervised learning', 15), ('reinforcement learning', 14), ('shot learning', 10), ('machine learning', 8), ('task learning', 6)],
-            2023: [('supervised learning', 14), ('reinforcement learning', 10), ('machine learning', 8), ('task learning', 8), ('shot learning', 7)]}
-    # res = getConfHotspotsEvo("AAAI", 2020)
-    describe(data, title="Hotspots Evolution in AAAI", xlabel="Year", ylabel="Count")    
+    
+    data = getConfHotspotsEvo("AAAI", 2014, top_k=5)
+    describe(data, title="Hotspots Evolution of AAAI in recent 10 years", xlabel="Year", ylabel="Count")    
+    # keyword = getConfHotspots("AAAI", 2020)
+
     
 
     
